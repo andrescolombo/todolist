5241aef8886102cb6fa7faf672b773b69719e53a
“Andres” <andres.colombo@gmail.com>
2021-05-01 10:27:52 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='Todo-App'>
+      <TodoList />
+      <h1>Todo App</h1>
+    </div>
+  );
+}
+
+export default App;
